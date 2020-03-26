92348a53e50ec54eb679faf1254e077c54dda0d8
Reno DuBois <renodubois@users.noreply.github.com>
2020-03-25 21:07:43 -0500

    Add library to handle SVGs, add search icon

---
+++ tabltop/icons/SearchIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Svg, Path } from "react-native-svg";
+
+interface SearchIconProps {
+    color: string;
+}
+
+function SearchIcon({ color }: SearchIconProps) {
+    return (
+        <Svg width={24} height={24} viewBox="0 0 24 24" fill="none">
+            <Path
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                stroke={color}
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </Svg>
+    );
+}
+
+export default SearchIcon;

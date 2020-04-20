e057cd2a4aca1352d068c664307e5016ae46968d
Reno DuBois <renodubois@users.noreply.github.com>
2020-04-20 11:08:11 -0500

    Add basic test for GameSearch

--- tabltop/__tests__/GameSearch-test.tsx
+++ tabltop/__tests__/GameSearch-test.tsx
@@ -1,14 +1,29 @@
+import { render } from "@testing-library/react-native";
 import React from "react";
-import renderer, { act } from "react-test-renderer";
 import GameSearch from "../components/GameSearch";
 
-it("renders properly", () => {
-	renderer.create(
+it("renders game items correctly", () => {
+	const testGames = [
+		{
+			id: "266192",
+			name: "Wingspan",
+			publisher: "Stonemaier Games",
+			yearPublished: 2019,
+			thumbnailURL:
+				"https://cf.geekdo-images.com/thumb/img/wvfZwwtcqpth4bgHnh4M-EhUCXg=/fit-in/200x150/pic4458123.jpg",
+			minPlayers: 1,
+			maxPlayers: 5,
+			categories: ["Card Game", "Set Collection"]
+		}
+	];
+	const { queryByRole } = render(
 		<GameSearch
-			games={[]}
+			games={testGames}
 			onGameSelect={jest.fn()}
 			onTextChange={jest.fn()}
-			query=""
+			query="wing"
 		/>
 	);
+	const gameItem = queryByRole("button");
+	expect(gameItem).toBeTruthy();
 });
